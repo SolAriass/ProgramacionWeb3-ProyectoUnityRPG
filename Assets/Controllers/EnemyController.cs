015eb0062160bba9463dc7550f63a664939840ca
JaviRecchia <javimolina073@gmail.com>
2025-10-28 09:56:44 -0300

    se agregaron los sprites de rick, el animator tiene toda la secuencia, faltaria ver las fuerzas para que salte, y agregar el rebote por daño

---
+++ Assets/Controllers/EnemyController.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform player;
+    public float detectionRadius = 5.0f;
+    public float speed = 2.0f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private bool enMovimiento;
+    private Animator animator;
+    SpriteRenderer sr;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distanceToPlayer < detectionRadius)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+
+            if (direction.x < 0)
+            {
+                sr.flipX = true;
+            }
+            if (direction.x > 0)
+            {
+                sr.flipX = false;
+            }
+
+            movement = new Vector2(direction.x, 0);
+
+            enMovimiento = true;
+        }
+        else 
+        {
+            movement = Vector2.zero;
+            enMovimiento = false;
+        }
+
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+
+        animator.SetBool("enMovimiento", enMovimiento);
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) 
+        {
+            Vector2 direccionDanio = new Vector2(transform.position.x, 0);
+            
+            collision.gameObject.GetComponent<MovimientoJugador>().RecibeDanio(direccionDanio, 1);
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+    }
+}
+
---
+++ Assets/Controllers/JugadorController.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovimientoJugador : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float fuerzaSalto = 30f;
+    public float fuerzaRebote = 15f;
+    public float longitudRaycast = 0.43f;
+    public LayerMask Suelo;
+
+    private bool enSuelo;
+    private bool recibiendoDanio;
+    public Rigidbody2D rb;
+
+    public float velocidad = 100f;
+    private Vector2 mover;
+    private PlayerInput playerInput;
+    [SerializeField] private Animator animator;
+
+    SpriteRenderer sr;
+
+
+    void Start()
+    {
+        // Tomamos la referencia al Player Input
+        playerInput = GetComponent<PlayerInput>();
+
+        // 🔧 Solución de la advertencia:
+        // Desactiva todos los mapas de acción globales
+        playerInput.actions.Disable();
+
+        // Activa solo el mapa de acciones "Jugador"
+        playerInput.actions.FindActionMap("Jugador").Enable();
+
+        rb = GetComponent<Rigidbody2D>();
+
+        sr = GetComponent<SpriteRenderer>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desplazamiento = new Vector3(mover.x, mover.y, 0f) * velocidad * Time.deltaTime;
+
+        animator.SetFloat("enMovimiento", mover.x * velocidad);
+
+        if (mover.x < 0)
+        {
+            sr.flipX = true;
+        }
+        if (mover.x > 0)
+        {
+            sr.flipX = false;
+        }
+
+        if (!recibiendoDanio)
+            transform.position += desplazamiento;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, longitudRaycast, Suelo);
+        enSuelo = hit.collider != null;
+
+        if (enSuelo && Input.GetKeyDown(KeyCode.Space) && !recibiendoDanio)
+        {
+            rb.AddForce(new Vector2(0f, fuerzaSalto), ForceMode2D.Impulse);
+        }
+
+        animator.SetBool("ensuelo", enSuelo);
+        animator.SetBool("recibeDanio", recibiendoDanio);
+
+    }
+    // Este método debe tener el mismo nombre que la acción "Mover" en el Input Actions asset
+    void OnMover(InputValue value)
+    {
+        mover = value.Get<Vector2>();
+    }
+
+    public void RecibeDanio(Vector2 direccion, int cantDanio)
+    {
+        if (!recibiendoDanio)
+        {
+            recibiendoDanio = true;
+
+            Vector2 rebote = new Vector2(transform.position.x - direccion.x, 1).normalized;
+            rb.AddForce(rebote * fuerzaRebote, ForceMode2D.Impulse);
+        }
+    }
+
+    public void DesactivaDanio()
+    {
+        recibiendoDanio = false;
+        rb.linearVelocity = Vector2.zero;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * longitudRaycast);
+    }
+
+    void FixedUpdate()
+    {
+        // opción A: velocidad (recomendada)
+        rb.linearVelocity = new Vector2(mover.x * velocidad, rb.linearVelocity.y);
+
+        // opción B: MovePosition (suave y sólido)
+        // Vector2 delta = new Vector2(mover.x, 0f) * velocidad * Time.fixedDeltaTime;
+        // rb.MovePosition(rb.position + delta);
+    }
+
+}
+
+/*
+Asegurate de que el nombre del método (OnMover) coincida exactamente con el nombre de la acción “Mover”.
+
+Si la acción tiene otro nombre (por ejemplo “Move”), cambiá el método a OnMove o On<NameAcción>.
+En versiones recientes de Unity se requiere que coincida exactamente.
+*/
